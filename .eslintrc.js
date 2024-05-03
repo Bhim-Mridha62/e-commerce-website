19817c4dee861cf2fa2be724b57e1548d324defc
Bhim Mridha <bhimmridha62@gmail.com>
2024-05-03 10:53:05 +0530

    update data

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
-
 module.exports = {
-  // Other ESLint configurations...
+  extends: ["@salesforce/eslint-config-lwc/recommended"], // Extend Salesforce LWC ESLint config
   rules: {
     // Other rules...
     "no-console": "off", // Disable the no-console rule
