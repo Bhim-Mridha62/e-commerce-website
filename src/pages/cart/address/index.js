4188b160be9559ccf74a49886f0e7cb75b8f0ad6
Bhim Mridha <bhimmridha62@gmail.com>
2024-05-14 09:03:16 +0530

    payment page

--- src/pages/cart/address/index.js
+++ src/pages/cart/address/index.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { DeliveryAddressSchema } from "@/Schemas/client/FormSchema";
+import {districts} from "@/utils/client/districts.js"
 const index = () => {
   const initialValues = {
-    name: '',
-    alternateName: '',
-    phone: '',
-    alternatePhone: '',
-    pincode: '',
-    state: '',
-    district: '',
-    Villege: '',
-    buildingAddress: '',
+    name: "",
+    alternateName: "",
+    phone: "",
+    alternatePhone: "",
+    pincode: "",
+    state: "Odisha",
+    district: "",
+    Villege: "",
+    buildingAddress: "",
   };
 
   const handleSubmit = (values, { resetForm }) => {
     alert(JSON.stringify(values, null, 2));
-    resetForm(); // Reset the form after submission
+    resetForm();
   };
   return (
     <div className="m-4">
@@ -29,185 +30,187 @@ const index = () => {
         onSubmit={handleSubmit}
       >
         <Form>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="name" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> Name
-            </label>
-            <Field
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Enter your name"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-300"
-            />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className="text-red-500 text-sm mt-1"
-            />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="name" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> Name
+              </label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Enter your name"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="alternateName"
+                className="text-gray-700 font-semibold"
+              >
+                Alternate Name
+              </label>
+              <Field
+                type="text"
+                id="alternateName"
+                name="alternateName"
+                placeholder="Enter alternate name"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="phone" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> Phone Number
+              </label>
+              <Field
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="Enter your phone number"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="alternatePhone"
+                className="text-gray-700 font-semibold"
+              >
+                Alternate Phone Number
+              </label>
+              <Field
+                type="text"
+                id="alternatePhone"
+                name="alternatePhone"
+                placeholder="Enter alternate phone number"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="pincode" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> Pin Code
+              </label>
+              <Field
+                type="text"
+                id="pincode"
+                name="pincode"
+                placeholder="Enter your pin code"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+              <ErrorMessage
+                name="pincode"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+            <div>
+              <label htmlFor="state" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> State
+              </label>
+              <Field
+                type="text"
+                id="state"
+                value="Odisha"
+                name="state"
+                placeholder="Select your state"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+              <ErrorMessage
+                name="state"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="district" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> District
+              </label>
+              <Field
+                as="select"
+                id="district"
+                name="district"
+                placeholder="Select your district"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              >
+                <option value="" disabled>
+                  --- Select District ---
+                </option>
+                {districts?.map((district) => (
+                  <option key={district} value={district}>
+                    {district}
+                  </option>
+                ))}
+              </Field>
+              <ErrorMessage
+                name="district"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
+            <div>
+              <label htmlFor="Villege" className="text-gray-700 font-semibold">
+                <span className="text-red-500">*</span> Villege
+              </label>
+              <Field
+                type="text"
+                id="Villege"
+                name="Villege"
+                placeholder="Enter your Villege"
+                className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
+              />
+              <ErrorMessage
+                name="Villege"
+                component="div"
+                className="text-red-500 text-sm mt-1"
+              />
+            </div>
           </div>
           <div>
             <label
-              htmlFor="alternateName"
+              htmlFor="buildingAddress"
               className="text-gray-700 font-semibold"
             >
-              Alternate Name
+              Building Address
             </label>
             <Field
-              type="text"
-              id="alternateName"
-              name="alternateName"
-              placeholder="Enter alternate name"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="phone" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> Phone Number
-            </label>
-            <Field
-              type="text"
-              id="phone"
-              name="phone"
-              placeholder="Enter your phone number"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+              as="textarea"
+              id="buildingAddress"
+              name="buildingAddress"
+              placeholder="Enter your building address"
+              rows="3"
+              className="border border-gray-300 text-black rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 "
             />
             <ErrorMessage
-              name="phone"
+              name="buildingAddress"
               component="div"
               className="text-red-500 text-sm mt-1"
             />
           </div>
-          <div>
-            <label
-              htmlFor="alternatePhone"
-              className="text-gray-700 font-semibold"
+          <div className="flex justify-center mt-6">
+            <button
+              type="submit"
+              // disabled={isSubmitting}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             >
-              Alternate Phone Number
-            </label>
-            <Field
-              type="text"
-              id="alternatePhone"
-              name="alternatePhone"
-              placeholder="Enter alternate phone number"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
+              Submit
+            </button>
           </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="pincode" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> Pin Code
-            </label>
-            <Field
-              type="text"
-              id="pincode"
-              name="pincode"
-              placeholder="Enter your pin code"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <ErrorMessage
-              name="pincode"
-              component="div"
-              className="text-red-500 text-sm mt-1"
-            />
-          </div>
-          <div>
-            <label htmlFor="state" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> State
-            </label>
-            <Field
-              as="select"
-              id="state"
-              name="state"
-              placeholder="Select your state"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="">Select your state</option>
-              <option value="state1">State 1</option>
-              <option value="state2">State 2</option>
-            </Field>
-            <ErrorMessage
-              name="state"
-              component="div"
-              className="text-red-500 text-sm mt-1"
-            />
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="district" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> District
-            </label>
-            <Field
-              as="select"
-              id="district"
-              name="district"
-              placeholder="Select your district"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              <option value="">Select your district</option>
-              <option value="district1">District 1</option>
-              <option value="district2">District 2</option>
-            </Field>
-            <ErrorMessage
-              name="district"
-              component="div"
-              className="text-red-500 text-sm mt-1"
-            />
-          </div>
-          <div>
-            <label htmlFor="Villege" className="text-gray-700 font-semibold">
-            <span className="text-red-500">*</span> Villege
-            </label>
-            <Field
-              type="text"
-              id="Villege"
-              name="Villege"
-              placeholder="Enter your Villege"
-              className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <ErrorMessage
-              name="Villege"
-              component="div"
-              className="text-red-500 text-sm mt-1"
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="buildingAddress"
-            className="text-gray-700 font-semibold"
-          >
-            Building Address
-          </label>
-          <Field
-            as="textarea"
-            id="buildingAddress"
-            name="buildingAddress"
-            placeholder="Enter your building address"
-            rows="3"
-            className="border border-gray-300 rounded-md py-2 px-3 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <ErrorMessage
-            name="buildingAddress"
-            component="div"
-            className="text-red-500 text-sm mt-1"
-          />
-        </div>
-        <div className="flex justify-center mt-6">
-          <button
-            type="submit"
-            // disabled={isSubmitting}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Submit
-          </button>
-        </div>
-      </Form>
+        </Form>
       </Formik>
     </div>
   );
--- src/pages/cart/address/payment/index.js
+++ src/pages/cart/address/payment/index.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Radio, Space, Divider, Button } from 'antd';
 
-function payment() {
+const PaymentOptions = () => {
   return (
-    <div>payment</div>
-  )
-}
+    <>
+   
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      <h2 className="text-xl font-bold mb-4 text-black">Payment Options</h2>
+      <Radio.Group className="w-full">
+        <Space direction="vertical" className="w-full">
+          <Radio value="upi" disabled className="w-full">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <span>Pay by any UPI app</span>
+              </div>
+            </div>
+          </Radio>
+          <Divider />
+          <Radio disabled value="card" className="w-full">
+            Credit / Debit / ATM Card
+            <span className="text-gray-500 block">Add and secure cards as per RBI guidelines</span>
+          </Radio>
+          <Divider />
+          <Radio disabled value="netbanking" className="w-full">
+            Net Banking
+            <span className="text-gray-500 block">This instrument has low success, use UPI or cards for better experience</span>
+          </Radio>
+          <Divider />
+          <Radio disabled value="emi" className="w-full">
+            EMI (Easy Installments)
+          </Radio>
+          <Divider />
+          <Radio value="cod" className="w-full">
+            Cash on Delivery
+          </Radio>
+        </Space>
+      </Radio.Group>
+    </div>
+      <div className='text-center mt-5'>
+        {/* <Button className='px-6 bg-[#3daf3d] text-xl'>CONFIRM ORDER</Button> */}
+        <button className='text-white border border-solid border-gray-400 px-8 py-4 text-lg rounded-md bg-gray-800'>CONFIRM ORDER</button>
+      </div>
+    </>
+  );
+};
 
-export default payment
+export default PaymentOptions;
