c018b664ed505535283de7e9c1557b3f61967a7d
Bhim Mridha <bhimmridha62@gmail.com>
2024-05-25 22:47:50 +0530

    check backend is host

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api");
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
   return (
     <Layout>
       <Component {...pageProps} />
