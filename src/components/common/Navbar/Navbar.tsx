0f8337066bed42b222f65032ca5a16dec57fb6f8
Bhim Mridha <bhimmridha62@gmail.com>
2024-12-12 22:53:19 +0530

    tset  1

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -165,6 +165,7 @@ const Navbar: React.FC = () => {
             <div onClick={() => router.push("/")} className="cursor-pointer">
               <Image src="/logo.png" alt="logo" height={40} width={100} />
             </div>
+            clshcisbc
             <AutoComplete
               // open={false}
               options={autoCompleteOptions.map((item) => ({
