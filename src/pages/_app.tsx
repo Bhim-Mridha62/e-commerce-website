1da0917e45a4f63a4703cff293652a88962abdb3
Bhim-Mridha62 <bhimmridha62@gmail.com>
2025-12-20 10:39:13 +0530

    feat: Add _app.tsx to configure global layout, styles, and head metadata.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-adsense-account" content="ca-pub-7430915642068204" />
         <meta charSet="UTF-8" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7430915642068204"
           crossOrigin="anonymous"></script>
